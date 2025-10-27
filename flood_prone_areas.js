5fbaadb9c955e1fb56f0b8b714305f009f79567f
koeir <kianjohnban@gmail.com>
2025-10-27 20:47:17 +0800

    small content change

--- flood_prone_areas.js
+++ flood_prone_areas.js
@@ -12,7 +12,7 @@ end:"Ongoing",
 status:"Very Flood-prone",
 coords:[10.67221450, 122.94435940],
 desc:"In 2023, now mayor Greg Gasataya announced his project to build a diversion channel at the reclamation area. The pictures below, taken on October 14, 2025, showcases the fruit of his efforts.",
-source:"https://www.pna.gov.ph/articles/1257376",
+source:"https://www.pna.gov.ph/articles/1257376\nPictures taken by locals.",
 images: [
     "images/sm_bacolod/sm_bacolod1.jpg",
     "images/sm_bacolod/sm_bacolod2.jpg",
