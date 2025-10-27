a68c44f0e725862fb2d3fa0289384e1d4ed8378d
koeir <kianjohnban@gmail.com>
2025-10-27 18:51:40 +0800

    minor revisions to bacolod project

--- projects.js
+++ projects.js
@@ -5,14 +5,14 @@ const PROJECTS = [
 // Visayas
 { 
 name: "Bacolod Diversion Channel / Drainage Works",
-area:"Bacolod City (Negros Occidental)",
+area:"SM Bacolod City",
 budget:"₱152M (+request)",
 start:"2024",
 end:"2025",
 status:"Ongoing",
 coords:[10.67221450, 122.94435940],
 desc:"Diversion channel and drainage improvements.",
-source:"Local news/DPWH" 
+source:"https://www.pna.gov.ph/articles/1257376" 
 },
 ]
 // Export for script.js loading (if using module bundler). Else script.js accesses PROJECTS variable.
